99a1c5ac53ddb7f0362e4d99a40252e8fa35c413
Marcel <halanocca21@gmail.com>
2020-08-18 09:42:27 -0500

    start proyect calendar app aadd login screen,navbar and login form

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+  } from "react-router-dom";
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+  
+
+export const AppRouter = () => {
+    return (
+            
+            <Router>
+                <div>
+                   <Switch>
+                        {/*    component={LoginScreen} */}
+                        <Route exact path="/login" component={LoginScreen}/>
+                            
+                        <Route path="/calendar" component={CalendarScreen}/>
+                            
+                        <Redirect to="/"/>
+                  
+                    </Switch>
+                </div>
+            </Router>
+    )
+}
