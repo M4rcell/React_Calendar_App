2da25552d7ccffd2595779f55b30b4525fabdb9c
Marcel <halanocca21@gmail.com>
2020-08-18 11:04:10 -0500

    configuraciones adcionales de calendar

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { AppRouter } from './routers/AppRouter'
+import { AppRouter } from './routers/AppRouter';
 
 export const CalendarApp = () => {
     return (
-        <div>
+        <>
             <AppRouter/>
-        </div>
+        </>
     )
 }
--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 
+
 import { Navbar } from '../ui/Navbar'
+import { messages } from '../../helpers/calendar-messages-es';
+
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 const events = [{
@@ -15,6 +20,24 @@ const events = [{
 ]
 
 export const CalendarScreen = () => {
+
+    const eneventStyleGetter =(event,start,end,isSelect)=>{
+       
+        console.log(event,start,end,isSelect);
+
+        const style={
+            background:'#367cF7',
+            borderRadius:'0px',
+            opacity:0.8,
+            display:'block',
+            color:'white'
+        }
+
+        return {
+            style 
+        }
+    };
+
     return (
         <div>
             <div className="calendar-screem">
@@ -26,6 +49,8 @@ export const CalendarScreen = () => {
                 startAccessor="start"
                 endAccessor="end"
                 style={{ height: 500 }}
+                messages={messages}
+                eventPropGetter={eneventStyleGetter}
                 />
                 </div>
         </div>
