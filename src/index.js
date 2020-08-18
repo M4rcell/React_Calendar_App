cacce8235fac487ee5940925dfdae81135179027
Marcel <halanocca21@gmail.com>
2020-08-18 11:11:43 -0500

    fixed bug in index :)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
 //import './styles.css';
 ReactDOM.render(
-  <React.StrictMode>
-    <CalendarApp/>
-  </React.StrictMode>,
+ 
+    <CalendarApp/>,
+  
   document.getElementById('root')
 );
 
